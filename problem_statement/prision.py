01c5775185ec7c7957fd11e1d02c9af3468d129b
paniraj93 <142389594+paniraj93@users.noreply.github.com>
2024-06-30 12:32:03 +0530

    delete

--- problem_statement/prision.py
+++
@@ -1,7 +0,0 @@
-p = int(input("Enter number of prisioners: "))
-c = int(input("Enter number of chocolates: "))
-s = int(input("Enter first number: "))
-sol = (s+c-1)%p
-if sol==0:
-    sol =6; 
-print(f"prisoner {sol} gets faulty chocolate")
